a01f056e2c2e9239612096379ded3b75f7621841
ngovanquoc4802000 <ngovanquoc480@gmail.com>
2023-08-16 19:42:02 +0700

    code hom qua

--- slick.slider/app.js
+++ slick.slider/app.js
@@ -1,32 +1,47 @@
 $(document).ready(function () {
   $(".product-slider").slick({
-    slidesToShow: 4,
     dots: true,
-    slidesToScroll: 2,
-    infinite: true,
-    lazyLoad: "ondemand",
-    autoplay: true,
-    autoplay : 1500,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     responsive: [
       {
-        breakpoint: 740,
+        breakpoint: 1024,
         settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: "100px",
-          slidesToShow: 1,
-        },
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
       },
-    ],
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+     
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 });
+
 
 $(document).ready(function () {
   $("main").slick({
     infinite: true,
     speed: 500,
-    autoplay: true,
-    autoplay : 1500,
     prevArrow: `<button type='button' class='slick-prev pull-left'><i class="fas fa-arrow-left"></i></button>`,
     nextArrow: `<button type='button' class='slick-next pull-right'><i class="fas fa-arrow-right"></i></button>`,
   });
