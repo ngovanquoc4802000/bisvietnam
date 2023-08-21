39be0e7f51457c1e010b6658fd4b87da1bf5d777
ngovanquoc4802000 <ngovanquoc480@gmail.com>
2023-08-21 17:19:04 +0700

    code ngay 21/8

--- slick.slider/app.js
+++ slick.slider/app.js
@@ -23,13 +23,33 @@ $(document).ready(function () {
         }
       },
       {
+        breakpoint: 540,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
         }
       },
-     
+      {
+        breakpoint: 412,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
