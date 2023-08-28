e605cca4d3f5988e0321c8b5ab742abc26cac5a3
ngovanquoc4802000 <ngovanquoc480@gmail.com>
2023-08-28 21:19:24 +0700

    code 28/8

--- product.js
+++ product.js
@@ -1,13 +1,26 @@
 const toggleSpan = document.querySelector(".toggle-header span");
 const headerToggle = document.querySelector(".header-toggle");
 const menu = document.querySelector(".icon-menu");
-console.log(menu);
-console.log(headerToggle);
+const myCarouselElement = document.querySelector('#myCarousel')
 
-menu.addEventListener("click",function() {
-    headerToggle.classList.toggle("show-siderbar");
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
+
+const myDropdown = document.querySelector('.dropdown')
+myDropdown.addEventListener('show.bs.dropdown', event => {
+   event.hidden.bs.dropdown	
+})
+
+menu.addEventListener("click", function () {
+  headerToggle.classList.toggle("show-siderbar");
 });
 
-toggleSpan.addEventListener("click",function() {
-    headerToggle.classList.remove("show-siderbar");
-})
+toggleSpan.addEventListener("click", function () {
+  headerToggle.classList.remove("show-siderbar");
+});
+/Image/tu-soi-mau-x-rite-SpectraLight-QC-Light-Booth-800x500-768x480 (1).gif
+./Image/hop-soi-mau-cac60-verivide.gif
+./Image/img10.jpg
+./Image/may-test-luc-nut.gif
